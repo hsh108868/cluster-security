233bd4523b0a834b24c8283b56ffedd844a391a2
hsh108868 <pusrty@gmail.com>
2022-05-20 02:56:14 +0900

    cluster describe

---
+++ Client/describe.py
@@ -0,0 +1,27 @@
+from unicodedata import name
+from urllib import response
+import boto3
+
+
+def describe_cluster(cluster_name):
+    eks = boto3.client('eks', region_name='ap-northeast-2')
+    print("EKS :", eks)
+    try:
+        response = eks.describe_cluster(name=cluster_name)
+    except Exception as e:
+        return None
+    return response['cluster']  # eks
+
+
+def main():
+    test_cluster_name: 'Cluster-Security'
+    result = describe_cluster(test_cluster_name)
+    if result is None:
+        print('Can`t gain about cluster information {}'.format(test_cluster_name))
+    else:
+        print('Cluster name'.format(result['name']))
+        print('Status'.format(result['status']))
+
+
+if __name__ == '__main__':
+    main()
