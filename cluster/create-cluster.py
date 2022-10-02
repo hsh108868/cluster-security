ffc9862c4d563b450be724fb3a7f43e4d3b5cff6
hsh108868 <pusrty@gmail.com>
2022-10-02 13:02:11 +0900

    ADD create cluster

--- cluster/create-cluster.py
+++ cluster/create-cluster.py
@@ -4,6 +4,15 @@ import pulumi_aws as eks
 import pulumi_kubernetes as k8s
 
 
-cluster = eks.Cluster("eks-cluster")
+cluster = eks.Cluster("eks-cluster", eks.ClusterArgs(
+    desired_capacity=2
+))
+
+
+node_group = eks.NodeGroup('eks-cluster-group',
+                           cluster=cluster.core,
+                           instance_type='t2.micro',
+                           desired_capacity=1
+                           )
 
 pulumi.export("kubeconfig", cluster.kubeconfig)
