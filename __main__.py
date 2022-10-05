abffc2f7d016f4a35eb91ff61c8acd4d21c6b197
hsh108868 <pusrty@gmail.com>
2022-10-05 22:48:04 +0900

    test

--- __main__.py
+++ __main__.py
@@ -3,7 +3,6 @@
 import pulumi
 from pulumi_aws import s3
 import cluster.create_cluster as cluster
-import Image_Vulnerability.docker as docker
 
 # Create an AWS resource (S3 Bucket)
 bucket = s3.Bucket('pod-list')
@@ -11,4 +10,3 @@ bucket = s3.Bucket('pod-list')
 # Export the name of the bucket
 pulumi.export('pod-list', bucket.id)
 cluster.create_cluster()
-docker.docker()
